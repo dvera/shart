cc6a83d870d011205e9135ebfe10ac83c66b42d5
Daniel Vera <vera@genomics.fsu.edu>
2017-06-15 23:42:47 -0400

    Update make_index.sh

--- scripts/make_index.sh
+++ scripts/make_index.sh
@@ -10,19 +10,3 @@ FADIR=$(readlink -f $GENOMEFA)
 
 IDXFILES="${PREFIX}.amb ${PREFIX}.ann ${PREFIX}.bwt ${PREFIX}.pac ${PREFIX}.sa"
 (cd $FADIR && tar -czf $CURDIR/${PREFIX}_idx.tar.gz $IDXFILES && rm $IDXFILES)
-
-awk '{
-  if($0~\">\"){
-    if(length(chrom)!=0){
-      print chrom,clen
-    };
-    chrom=$1;
-    clen=0
-   } else{
-    clen+=length($0)
-   }
-  } END{
-    print chrom,clen
-  }' OFS='\t' $GENOMEFA | tr -d '>' | sort -k1,1 > ${CURDIR}/${PREFIX}.chrom.sizes
-  
-  
