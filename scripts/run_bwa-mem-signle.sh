4a458b8e9932df3d2ccd92c78c3a37fa80094b0f
Daniel Vera <vera@genomics.fsu.edu>
2017-06-16 00:30:54 -0400

    Delete run_bwa-mem-signle.sh

--- scripts/run_bwa-mem-signle.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-INPUT=$1
-INDEX=$2
-PREFIX=$3
-NTHREADS=$4
-
-run-bwa-mem-single.sh $INPUT $INDEX $PREFIX $NTHREADS run-sort-bam.sh $INPUT $PREFIX samtools stats $INPUT > ${INPUT}.samstats
