7d625c9c719b272f0830172cb65d1a33233baafc
Daniel Vera <vera@genomics.fsu.edu>
2017-06-16 07:22:06 -0400

    Update run_bedtools-intersect.sh

--- scripts/run_bedtools-intersect.sh
+++ scripts/run_bedtools-intersect.sh
@@ -6,4 +6,4 @@ PREFIX=$3
 
 bedtools bamtobed -i $INPUT | cut -f 1,2,3,4,5,6 | sort -k1,1 -k2,2n > ${PREFIX}.bed
 SCALE=$(echo "1000000/$(wc -l $INPUT | cut -d" " -f1)" | bc -l)
-bedtools intersect -sorted -c -b $INPUT -a $WINDOWBED | awk -v SCALE=$SCALE '{print $1,$2 ,$3 ,$4*SCALE }' OFS='\t' > ${PREFIX}.bg
+bedtools intersect -sorted -c -b ${PREFIX}.bed -a $WINDOWBED | awk -v SCALE=$SCALE '{print $1,$2 ,$3 ,$4*SCALE }' OFS='\t' > ${PREFIX}.bg
