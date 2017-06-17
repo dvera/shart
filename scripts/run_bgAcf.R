2cf11ba79a2ba8087e83aab4f8b0edb9b3a26453
Daniel Vera <vera@genomics.fsu.edu>
2017-06-16 21:47:25 -0400

    Update run_bgAcf.R

--- scripts/run_bgAcf.R
+++ scripts/run_bgAcf.R
@@ -2,4 +2,5 @@
 
 args = commandArgs(trailingOnly=TRUE)
 df = read.table(args[1], stringsAsFactors=FALSE)
-cat(acf(df[,4],lag.max=1,plot=F)[[1]][2],"\n")
+write(acf(df[,4],lag.max=1,plot=F)[[1]][2],paste0(args[1],".acf"))
+
