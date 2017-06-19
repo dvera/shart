8a0d5b661e34ec41ca108717a45e526b49de4549
Daniel Vera <vera@genomics.fsu.edu>
2017-06-19 09:58:57 -0400

    Update run_quantile-normalization.sh

--- scripts/run_quantile-normalization.sh
+++ scripts/run_quantile-normalization.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 INPUT=$1
+RTBG=${@:3}
 REFERENCE=$2
-PREFIX=$3
 
-BGLINES=$(wc -l $INPUT | cut -d" " -f 1)
-paste <(sort -T . -k4,4g $INPUT) <(shuf -n $BGLINES $REFERENCE | sort -k4,4g) | cut -f 1,2,3,8 | sort -T . -k1,1 -k2,2n > ${PREFIX}_qnorm.bg
+for INPUT in $RTBG; do
+  OUTPUT=$(basename $INPUT | sed 's/\.bg/_qnorm\.bg/g')
+  BGLINES=$(wc -l $INPUT | cut -d" " -f 1)
+  paste <(sort -T . -k4,4g $INPUT) <(shuf -n $BGLINES $REFERENCE | sort -k4,4g) | \
+    cut -f 1,2,3,8 | sort -k1,1 -k2,2n > $OUTPUT
+done
