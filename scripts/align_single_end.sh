f24191e88c5f018cb7ac9953f93b746c4036e9bf
Daniel Vera <vera@genomics.fsu.edu>
2017-06-19 14:20:51 -0400

    Update align_single_end.sh

--- scripts/align_single_end.sh
+++ scripts/align_single_end.sh
@@ -1,22 +1,69 @@
 #!/bin/bash
 
-fastq=$1
-index_file=$2
-prefix=$3
-nThreads=$4
+#fastq=$1
+#index_file=$2
+#prefix=$3
+#nThreads=$4
 
-# unzip index
-tar -xzf $index_file
-index=`ls -1 *.bwt | head -1 | sed 's/\.bwt//g'`
+while getopts ":i:t:" opt; do
+  case $opt in
+    t)
+      #echo "-t was triggered, Parameter: $OPTARG" >&2
+      NTHREADS=$OPTARG
+      ;;
+    i)
+      INDEXFILE=$OPTARG
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
-# unzip fastq files
-if [[ $fastq =~ \.gz$ ]]
-then
-  gunzip $fastq
-  fastq=${fastq/%\.gz/}
+shift $((OPTIND-1))
+
+FASTQFILES=$@
+
+if [[ -f ${INDEXFILE}.bwt ]]; then
+  INDEXPREFIX=$INDEXFILE
+elif [[ -f $INDEXFILE && $INDEXFILE=~ \.tar\.gz$ ]]; then
+  mkdir -p bwaIndex
+  INDEXPATH=$(readlink -f $INDEXFILE)
+  tar -C bwaIndex -x -f $INDEXPATH
+  INDEXPREFIX=$(readlink -f bwaIndex/*.bwt | sed 's/\.bwt$//g')
+else
+  echo "index not found"
+  exit 2
 fi
 
 # run bwa
-bwa mem -t $nThreads $index $fastq | samtools view -Shb - > $prefix.bam
-
-samtools stats ${prefix}.bam > ${prefix}.bam.samstats
+for f in $FASTQFILES; do
+  # unzip file if compressed
+  if [[ ! -f $f ]]; then
+    echo "fastq file not found"
+    exit 1
+  elif [[ $f =~ \.gz$ ]]
+    fo=${fastq/%\.gz/}
+    gunzip -c $f > $fo
+    f=$fo
+  fi
+  
+  # create output prefix
+  OUTPUT="$(basename $f | sed 's/\.fq$//g' | sed 's/\.fastq$//g').bam"
+  
+  # check to see input name has same as output name
+#   if [[ $OUTPUT == $f ]]; then
+#     echo "fastq file has inappropriate name, must be a fastq"
+#     exit 1
+#   fi
+  
+  # align fastq file and run samstats
+  bwa mem -t $NTHREADS $INDEXPREFIX $f | samtools view -Shb - > $OUTPUT
+  samtools stats $OUTPUT > ${OUTPUT}.samstats
+ done
+ 
+ 
