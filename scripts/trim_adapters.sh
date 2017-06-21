0fd91d5563d297d275204ef3610edeb4dae2d990
Daniel Vera <vera@genomics.fsu.edu>
2017-06-21 10:36:26 -0400

    Update trim_adapters.sh

--- scripts/trim_adapters.sh
+++ scripts/trim_adapters.sh
@@ -1,7 +1,61 @@
 #!/usr/bin/env bash
 
-INPUT=$1
-PREFIX=$2
+usage() {
+  echo "Usage: $(basename $0) [-t threads ] fastq1 [fastq2 ... fastqN ]" 1>&2
+  echo "" 1>&2
+  exit 1
+}
 
-cutadapt -a AGATCGGAAGAGCACACGTCTG -q 0 -O 1 -m 0 -o ${PREFIX}_clip.fastq $INPUT > ${PREFIX}_clip.fastq.log
-fastqc ${PREFIX}_clip.fastq
+while getopts ":t:" opt; do
+  case $opt in
+  t)
+    NTHREADS=$OPTARG
+   ;;
+  \?)
+   echo "Invalid option: -$OPTARG" >&2
+   usage
+   ;;
+  [?])
+   usage
+   ;;
+  :)
+   echo "Option -$OPTARG requires an argument." >&2
+   echo "" >&2
+   usage
+   ;;
+  esac
+done
+
+if [ -z $NTHREADS ]; then
+  NTHREADS=1
+fi
+
+shift $((OPTIND-1))
+
+if [[ $# -eq 0 ]] ; then
+  echo 'no fastq files specified'
+  exit 1
+fi
+
+# DEBUG
+
+FASTQFILES=$@
+
+# check if all files exist
+for f in $FASTQFILES; do
+  if [[ ! -f $f ]]; then
+    echo "fastq file \"$f\" not found"
+    exit 1
+  fi
+done
+
+
+for f in $FASTQFILES; do
+ OUTFILE="$(basename $f | sed 's/\.gz$//g' | sed 's/\.fq$//g' | sed 's/\.fastq$//g')_clip.fastq"
+ LOGFILE=${OUTFILE}.log
+ echo "cutadapt -a AGATCGGAAGAGCACACGTCTG -q 0 -O 1 -m 0 -o $OUTFILE $f > $LOGFILE && fastqc $OUTFILE"
+done 
+
+
+ 
+ 
