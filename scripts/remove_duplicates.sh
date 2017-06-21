45dc2d3b0b9b1e497d3e46d809c345851f09db87
Daniel Vera <vera@genomics.fsu.edu>
2017-06-21 12:03:50 -0400

    Update remove_duplicates.sh

--- scripts/remove_duplicates.sh
+++ scripts/remove_duplicates.sh
@@ -1,16 +1,77 @@
 #!/usr/bin/env bash
 
-INPUT=$1
-OUTPUT=$(basename $INPUT | sed 's/\.bam$/_rmdup\.bam/g')
+usage() {
+  echo "Usage: $(basename $0) [-t threads ] fastq1 [fastq2 ... fastqN ]" 1>&2
+  echo "" 1>&2
+  exit 1
+}
 
-samtools rmdup -s $INPUT $OUTPUT
+while getopts ":t:" opt; do
+  case $opt in
+  t)
+    NTHREADS=$OPTARG
+   ;;
+  \?)
+   echo "Invalid option: -$OPTARG" >&2
+   usage
+   ;;
+  [?])
+   usage
+   ;;
+  :)
+   echo "Option -$OPTARG requires an argument." >&2
+   echo "" >&2
+   usage
+   ;;
+  esac
+done
 
-samtools stats $OUTPUT > ${OUTPUT}.samstats
+if [ -z $NTHREADS ]; then
+  NTHREADS=1
+fi
 
-SAMLINES=$(samtools view -c $INPUT)
+shift $((OPTIND-1))
 
-RSAMLINES=$(samtools view -c $OUTPUT)
+if [[ $# -eq 0 ]] ; then
+  echo 'no bam files specified'
+  exit 1
+fi
 
-PCTNONZEROES=$(echo "${RSAMLINES}/${SAMLINES}" | bc -l)
+# DEBUG
 
-echo "${PCTNONZEROES} of the ${SAMLINES} reads were kept after removing duplicates" | tee ${OUTPUT}.log
+FASTQFILES=$@
+
+# check if all files exist
+for f in $FASTQFILES; do
+  if [[ ! -f $f ]] ; then
+    echo "bam file \"$f\" not found"
+    exit 1
+  fi
+done
+
+# check if all files exist
+for f in $FASTQFILES; do
+  if [[ $f != *.bam ]]; then
+    echo "file \"$f\" is not a bam file"
+    exit 1
+  fi
+done
+
+echo "removing duplicates"
+for f in $FASTQFILES; do
+  OUTPUT=$(basename $f | sed 's/\.bam$/_rmdup/g')
+  echo "samtools rmdup -s $f $OUTPUT && samtools stats ${OUTPUT}.bam > ${OUTPUT}.bam.samstats"
+done | parallel --citation -j $NTHREADS
+
+for f in $FASTQFILES; do
+  OUTPUT=$(basename $f | sed 's/\.bam$/_rmdup\.bam/g')
+  #echo "SAMLINES=$(samtools view -c $f);RSAMLINES=$(samtools view -c $OUTPUT);PCTNONZEROES=$(echo "${RSAMLINES}/${SAMLINES}" | bc -l)
+  echo $(echo "$(samtools view -c $OUTPUT)/$(samtools view -c $f)" | bc -l) > ${OUTPUT}.pct
+done | parallel --citation -j $NTHREADS
+
+  
+
+
+
+ 
+ 
